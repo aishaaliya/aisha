57f921a16c834e873d9f3edc741d91f6c0146cf9
aisha <aishaaliya@icloud.com>
2022-12-22 13:06:48 +0500

    20dars

---
+++ 2-oy/20dars/main.js
@@ -0,0 +1,132 @@
+// let k =147
+// let D =147 % 7;
+//  D
+
+
+// satrlarni birlawtiriw -> CONTACTENATION
+
+// let a = 5
+// console.log(a>0 ?' Musbat ' : 'Manfiy');
+
+// console.log(!(5>3));
+
+
+// console.log(a %2 ===1 ? 'Toq son': 'juft son');
+
+
+//1
+// let a= 2
+// console.log(a>0 ?'Rost ' :'Xato');
+
+//2
+// let a= 143
+// console.log(a %2 ===1 ? 'Toq son': 'juft son');
+
+//3
+// let a= 143
+// console.log(a %2 ===1 ? 'Toq son': 'juft son');
+
+// 4
+// let a =- 4
+// let b= 5
+// console.log( a>2 &&b<=3 ?'Rost ' :'Xato');
+
+//5
+// let A = 2
+// let B = 6
+// console.log(A>=0 || B<-2 ?'Rost ' :'Xato');
+
+//6
+// let a = 8
+// let b = 5
+// let c = 9
+// console.log(a<=b || b<=c ?'Rost ' :'Xato');
+
+
+//let 7
+// let A = 4
+// let B = 1
+// let C = 9
+// console.log();
+
+
+// console.log( a>2 &&b<=3 ?'Rost ' :'Xato');
+
+//8
+// let a=3
+// let b=1
+// console.log(a%2 ==1 && b %2 ==1 );
+
+
+//9
+//  let a = 7
+//  let b = 9
+//  console.log(a%2== 0 || b%2 == 0);
+
+
+
+//10
+// let a= 6
+// let b= 12
+// console.log(a%2== 1 || b%2 ==0 );
+
+
+// //11
+// let a= 21
+// let b= 43
+// console.log();
+
+
+//12
+//  let a= 4
+//  let b= 9
+//  let c= 3
+//   console.log(a>=0 && b>=0 && c>=0);
+
+//13
+//   let a= 9
+//   let b= -9
+//   let c= -3
+//  console.log(a>=0 || b>=0 || c>=0 );
+
+//14
+//  let a= 9
+//  let b= -9
+//  let c= -3
+// console.log(a>=0 || b>=0 && c>=0 );
+
+
+//15
+// let a = 9
+// let b = 9
+// let c = 3
+// console.log(a >= 0 && b >= 0 || c >= 0);
+
+//16
+// let a= 11
+// console.log( (a>=9 || a>100) &&  a%2==0 );
+
+
+//17
+// let a= 120
+// console.log( (a>99 && a==100) ||  a%2==1 );
+
+//18
+// let a = 1
+// let b = 2
+// let c = 1
+// console.log(
+//     (a == b) ||
+//     (b == c) ||
+//     (a == c)
+// );
+
+//19
+let a= 4
+let b= 1
+let c= 9
+console.log(
+
+);
+
+
